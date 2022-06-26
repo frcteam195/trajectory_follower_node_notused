a859f1fd700c5081dc0c5c31891ad9e1e8f9c881
= <=>
2022-06-26 02:06:49 +0000

    Begin adding get trajectory service call

--- src/trajectory_follower_node.cpp
+++ src/trajectory_follower_node.cpp
@@ -1,24 +1,42 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+#include "trajectory_generator_node/GetTrajectory.h"
+
 #include <thread>
 #include <string>
 #include <mutex>
+#include <vector>
 
 ros::NodeHandle* node;
 
+ros::ServiceClient get_trajectory_service;
+
+ros::ServiceClient& get_trajectory_service_get()
+{
+	if (node && !get_trajectory_service)
+	{
+		get_trajectory_service = node->serviceClient<trajectory_generator_node::GetTrajectory>("get_trajectory", true);
+	}
+	return get_trajectory_service;
+};
+
+void get_trajectory(std::string trajectory_name)
+{
+	if (get_trajectory_service_get())
+	{
+		trajectory_generator_node::GetTrajectory gt;
+		gt.request.path_name = trajectory_name;
+		bool service_success = get_trajectory_service_get().call(gt);
+		if (service_success)
+		{
+			//gt.response.
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
-	/**
-	 * The ros::init() function needs to see argc and argv so that it can perform
-	 * any ROS arguments and name remapping that were provided at the command line.
-	 * For programmatic remappings you can use a different version of init() which takes
-	 * remappings directly, but for most command-line programs, passing argc and argv is
-	 * the easiest way to do it.  The third argument to init() is the name of the node.
-	 *
-	 * You must call one of the versions of ros::init() before using any other
-	 * part of the ROS system.
-	 */
 	ros::init(argc, argv, "trajectory_follower_node");
 
 	ros::NodeHandle n;
